e3acc3c41d8e118350876d43a1e62f551b4c36cd
Jonathan Chasteen <jonathan.chasteen@live.com>
2018-02-05 13:36:34 -0500

    finish admin and auth

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var Admin = require("../models/admin");
+var Attrib = require("../models/attributes");
 var middleware = require("../middleware");
 
 var page = {};
@@ -13,11 +14,49 @@ var page = {};
 
 router.get("/", function (req, res) {
   if (req.isAuthenticated()) {
-    page.title = "Administration"
-    res.render("admin/index", { page: page });
+    Attrib.findOne({}, function (err, attrib) {
+      if (err) res.redirect("/admin/init");
+      else if (attrib === null) res.redirect("/admin/init");
+      else {
+        page.title = "Administration";
+        res.render("admin/index", { page: page, attrib: attrib });
+      }
+    });
   } else {
     res.redirect("/admin/login");
   }
+});
+
+//
+// Set Site Attributes
+//
+// Initialize
+router.get("/init", function (req, res) {
+  page.title = "Initialize Site Attributes";
+  res.render("admin/init", { page, page });
+});
+router.post("/init", function (req, res) {
+  Attrib.create(req.body, function (err, newAttrib) {
+    if (err) console.log(err);
+    else {
+      res.redirect("/admin");
+    }
+  });
+});
+// Set if exists
+router.get("/set", middleware.isLoggedIn, function (req, res) {
+  page.title = "Set Site Attributes";
+  Attrib.findOne({}, function (err, attrib) {
+    if (err) console.log(err);
+    else if( attrib === null ) res.render("admin/init", { page, page });
+    else res.render("admin/set", { page: page, attrib: attrib });
+  });
+});
+router.put("/set", function (req, res) {
+  Attrib.findOneAndUpdate({}, req.body.attrib, function (err, attrib) {
+    if (err) console.log(err);
+    else res.redirect("/admin");
+  });
 });
 
 //
